1390271e2d12f20c74266a697e3f7d59028bf0e2
Richard Lirio <richardlirio@hotmail.com>
2025-07-09 16:30:15 -0300

    test: test delete vehicle created

---
+++ src/modules/vehicle/delete.vehicle.controller.e2e.spec.ts
@@ -0,0 +1,82 @@
+import request from "supertest";
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
+import { cleanupTestDatabase, setupTestDatabase } from "test/e2e-setup";
+import { buildApp } from "@/app";
+import { FastifyInstance } from "fastify";
+import { prisma } from "@/lib/database";
+import { hashPassword } from "@/utils/hash-password";
+
+describe("Delete Vehicle Controller (e2e)", async () => {
+  let application: FastifyInstance;
+  beforeAll(async () => {
+    application = await buildApp();
+    application.ready();
+    await setupTestDatabase();
+    await prisma.user.create({
+      data: {
+        name: "Admin user",
+        email: "admin@admin.com",
+        password: await hashPassword("123456"),
+        role: "ADMIN",
+      },
+    });
+  });
+
+  afterAll(async () => {
+    await application.close();
+    await cleanupTestDatabase();
+  });
+
+  async function geraCookies() {
+    const loginResponse = await request(application.server)
+      .post("/api/v1/login")
+      .send({
+        email: "admin@admin.com",
+        password: "123456",
+      });
+
+    const cookies = loginResponse.headers["set-cookie"];
+
+    if (!cookies) {
+      throw new Error("Cookie não encontrado após login");
+    }
+
+    return cookies;
+  }
+
+  it("should be able to delete a vehicle", async () => {
+    const cookies = await geraCookies();
+
+    const client = await prisma.client.create({
+      data: {
+        name: "JOHN DOE CLIENT",
+        cpfCnpj: "47022391041",
+        phone: "27997876754",
+        email: "johndoe@example.com",
+        address: "Rua nova, numero 2, Vitoria-ES",
+      },
+    });
+
+    const vehicle = await prisma.vehicle.create({
+      data: {
+        plate: "PPW1020",
+        model: "ARGO",
+        brand: "FIAT",
+        kilometers: 10000,
+        year: 2017,
+        clientId: client.id,
+      },
+    });
+
+    const response = await request(application.server)
+      .delete(`/api/v1/vehicles/${vehicle.id}`) //pelo id do usuario
+      .set("Cookie", cookies);
+    expect(response.statusCode).toEqual(204);
+
+    const vehicleExist = await prisma.vehicle.findUnique({
+      where: { id: vehicle.id },
+    });
+
+    expect(vehicleExist).toBeNull();
+  });
+});
